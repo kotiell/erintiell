feb4b6b6d012e74b1cd4b8270cd1f08c1b167ee4
kotiell <tielljustin@gmail.com>
2022-12-17 20:57:03 -0500

    mobile ready

--- app/header.tsx
+++ app/header.tsx
@@ -3,17 +3,17 @@ import FunPic from "../public/images/fun-portrait.jpg";
 
 export default function Header() {
   return (
-    <header className="container mx-auto my-8">
-      <div className="grid grid-cols-12 gap-8">
-        <div className="col-span-3">
+    <header className="container mx-auto my-8 px-4 md:px-0">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-8">
+        <div className="cols-span-12 md:col-span-3 ">
       <Image
         src={FunPic}
         alt="Erin Tiell"
         className="rounded-md"
       />
         </div>
-        <div className="col-span-5 flex flex-col justify-center">
-          <h1 className="text-8xl mt-4 font-extralight">Erin Tiell</h1>
+        <div className="cols-span-12 md:col-span-5 flex flex-col justify-center">
+          <h1 className=" text-6xl md:text-8xl mt-4 font-extralight">Erin Tiell</h1>
           <p className="text-2xl font-light">Marketing and communications professional with more than 10 years of proven results.</p>
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -25,35 +25,35 @@ export default async function Home() {
   const titleStyle = "font-light text-3xl";
   const paraStyle = "mb-2.5 leading-tight";
   return (
-    <div className="container">
+    <div className="container px-4 md:px-0">
       <div className="my-16">
-        <h3 className="font-bold text-4xl">Campaign architect, communications conduit, detail spelunker.</h3>
-        <p className="text-2xl font-light ">I am a self-starting, fastidious individual. I thrive in a team environment, can work independently and also carry great project management skills.</p>
+        <h3 className="font-bold text-2xl md:text-4xl leading-tight">Campaign architect, communications conduit, detail spelunker.</h3>
+        <p className="text-lg font-light ">I am a self-starting, fastidious individual. I thrive in a team environment, can work independently and also carry great project management skills.</p>
       </div>
 
-      <h2 className="text-4xl mt-8 mb-3 font-bold"> Past Work</h2>
+      <h2 className="text-3xl md:text-4xl mt-8 mb-3 font-bold"> Past Work</h2>
 
-      <div className="grid mt-1 gap-4">
+      <div className="grid mt-1 gap-6 md:gap-4">
         <div className="grid grid-cols-12">
-          <div className="self-center justify-self-center">
+          <div className="col-span-2 md:col-span-1 self-center justify-self-center pr-4 md:p-0">
             <Image
               src={Hub}
               alt="hub"
             />
           </div>
-          <div className="col-span-11">
+          <div className="col-span-10 md:col-span-11">
             <h3 className={titleStyle}>2016-present</h3>
             <p className={paraStyle}>Align and drive company strategy and external brand preference through effective communications and marketing project execution.</p>
           </div>
         </div>
         <div className="grid grid-cols-12">
-          <div className="self-center justify-self-center">
+          <div className="col-span-2 md:col-span-1 self-center justify-self-center pr-4 md:p-0">
             <Image
               src={Insights}
-              alt="hub"
+              alt="Insights"
             />
           </div>
-          <div className="col-span-11">
+          <div className="col-span-10 md:col-span-11">
             <h3 className={titleStyle}>2011-2016</h3>
             <p className={paraStyle}>Communicated to our customers to drive sales leads and improve profit. </p>
             <p className={paraStyle}>Used creative methods to deliver communications to 21,000 employees through traditional and innovating channels.</p>
@@ -61,13 +61,13 @@ export default async function Home() {
         </div>
         <div>
           <div className="grid grid-cols-12">
-            <div className="self-center justify-self-center">
+            <div className="col-span-2 md:col-span-1 self-center justify-self-center pr-4 md:p-0">
               <Image
                 src={Awesome}
-                alt="hub"
+                alt="Awesome"
               />
             </div>
-            <div className="col-span-11">
+            <div className="col-span-10 md:col-span-11">
               <h3 className={titleStyle}>2007-2011</h3>
               <p className={paraStyle}>Provided developmental support to the Executive Director in multiple and various roles to our local chapter of the American Red Cross. </p>
               <p className={paraStyle}>Aided our chapter to become one of three chapters in the state of Ohio to receive the Highly Performing Chapter award for the 2008-2009 fiscal year.</p>
